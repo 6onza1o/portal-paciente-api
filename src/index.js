c61b536db0f41324b52bbeb34a79acf33da39afe
Gonzalo E. Espinoza Osorio <gonzalo.espinoza@hbl.local>
2025-11-17 16:41:36 -0300

    primer push api

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import express, { json } from 'express';
 import cors from 'cors';
-import router from "./routes/index.routes.js";
+import router from './routes/index.routes.js';
 
 const app = express();
 
-// CORS para LOCAL y PRODUCCIÓN
-app.use(cors({
-  origin: [
-    'http://localhost:4200',
-    'https://portal-paciente-front-44c5.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
-
-
+app.use(cors());
 app.use(json());
-
-// Rutas
 app.use('/api', router);
 
-// Puerto
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-});
+// Solo local
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`🚀 Servidor local en http://localhost:${PORT}`);
+    });
+}
+
+export default app;

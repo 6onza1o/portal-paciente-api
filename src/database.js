db6824b669d0adfcb717939408f22fa865e2ed86
Gonzalo E. Espinoza Osorio <gonzalo.e.espinoza.o@gmail.com>
2025-12-30 09:52:34 -0300

    new contraseña api

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ export const pool = new pg.Pool({
   host: "aws-1-us-east-1.pooler.supabase.com",
   port: 6543,
   user: "postgres.gyqkjkpledxbsuraviuz",                     // tu usuario de supabase (completo)
-  password: "aT4zJDZBDnKoWKLD",                    // tu clave BD
+  password: "iGVCOQvPNLvbUDLz",                    // tu clave BD
   database: "postgres",
   ssl: { rejectUnauthorized: false }
 });
